924159de15c21c4dd7bb281f82ca900a66c9797d
kirbec <lolkirbec@gmail.com>
2019-01-25 11:05:24 -0600

    Refactoring in infoDetailViewController

--- DatabaseSpreadsheet/InformationSheetFiles/InfoFiles/InfoDetailViewController.swift
+++ DatabaseSpreadsheet/InformationSheetFiles/InfoFiles/InfoDetailViewController.swift
@@ -126,6 +126,12 @@ class InfoDetailViewController: UIViewController, UITableViewDataSource, UITable
         return 100
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+        }
+    }
+    
     func addSection() {
         let alert = UIAlertController(title: "Add New Section", message: nil, preferredStyle: .alert)
         let confirmAction = UIAlertAction(title: "Add", style: .default) { (_) in
@@ -159,7 +165,7 @@ class InfoDetailViewController: UIViewController, UITableViewDataSource, UITable
     }
     
     func exportInfoData() {
-        createCSVStringFromInfo(data: sections, estimateNum: 0)
+        CSVFile.createCSVStringFromInfo(data: sections, estimateNum: 0, curViewController: self)
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
@@ -273,45 +279,6 @@ class InfoDetailViewController: UIViewController, UITableViewDataSource, UITable
         estimateCostLabel.text = "Estimate Cost: $\(estimateTotal)"
         totalCostLabel.text = "Total Cost: $\(actualTotal)"
     }
-    
-    func createCSVStringFromInfo(data : [(String, [InfoProduct])], estimateNum : Int) {
-        let fileName = "Tasks.csv"
-        let path = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(fileName)
-        var csvText = ""
-        csvText += createHeader(estimateNum: estimateNum)
-        
-        do {
-            try csvText.write(to: path!, atomically: true, encoding: String.Encoding.utf8)
-            let vc = UIActivityViewController(activityItems: [path], applicationActivities: [])
-            if let popoverController = vc.popoverPresentationController {
-                popoverController.barButtonItem = self.navigationItem.rightBarButtonItem
-            }
-            present(vc, animated: true, completion: nil)
-        } catch {
-            print("Failed to create file")
-            print("\(error)")
-        }
-    }
-    
-    func createHeader(estimateNum : Int) -> String {
-        var headerString = ""
-        
-        headerString += "R Drewno Electric Inc, , , Estimate, #\(estimateNum), Date:, 10/28/1998\n"
-        headerString += "1216 S. Summit St\n"
-        headerString += "Barrington IL 60010, , Service ordered by: , COMPANY NAME\n"
-        headerString += "Tel. 847-791-6368, , , telephone:\n"
-        headerString += "Email: RDrewnoElectric@sbcglobal.net, , , email:\n\n"
-        headerString += "Job Description: , , JOB DESCRIPTION GOES HERE\n\n"
-        headerString += " , , , , ESTIMATE, , AS BUILT\n"
-        headerString += "Room, , Description, Unit Price, QTY, Total, QTY, Total\n"
-        
-        return headerString
-    }
-    
-    func createBody(data : [(String, Int)]) -> String {
-        var csvString = ""
-        
-        return csvString
-    }
+
     
 }
