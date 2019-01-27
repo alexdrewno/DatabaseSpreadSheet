9d213189359737385e68539ae70abd527668883d
kirbec <lolkirbec@gmail.com>
2019-01-26 18:23:03 -0600

    addedCompletedViewControllerFile

---
+++ DatabaseSpreadsheet/InformationSheetFiles/CompletedFiles/CompletedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CompletedViewController.swift
+//  DatabaseSpreadsheet
+//
+//  Created by Alex Drewno on 1/26/19.
+//  Copyright © 2019 Alex Drewno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CompletedViewController: UIViewController {
+    
+    
+    
+}
